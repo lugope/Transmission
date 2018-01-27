1af9ba261367ca3122803337076eb5fe1364d933
lugope <lucasgpereira.10@gmail.com>
2018-01-27 04:09:41 -0200

    First mechanics.

---
+++ Transmission/Assets/Character/Movement.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Movement : MonoBehaviour {
+
+	public float jumpForce = 6f;
+	public float movementForce = 3f;
+	public LayerMask groundLayer;
+	public Animator animator;
+	public bool isDead = false;
+
+	private Timer timer;
+	private Rigidbody2D rigidBody;
+
+
+	void Awake(){
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+	void Start () {
+		initiatePlayer ();
+	}
+
+	public void initiatePlayer(){
+		timer = GetComponent<Timer> ();
+		gameObject.layer = 0;
+		rigidBody.isKinematic = false;
+		timer.enabled = true;
+	}
+
+	public void die(){
+		Stop ();
+
+		gameObject.layer = 0;
+
+		enabled = false;
+		isDead = true;
+
+		rigidBody.isKinematic = true;
+
+		GetComponent<BoxCollider2D> ().enabled = false;
+		gameObject.GetComponentInChildren<TextMesh> ().text = "";
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+
+		// if is dead do not run update
+		if(isDead){
+			Debug.Log ("dead");
+			return;
+		}
+
+
+		// Input handle
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Move(Vector2.right * -1);
+		} else if ( Input.GetKeyDown (KeyCode.RightArrow) ){
+			Move(Vector2.right);
+		}
+
+		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+			Stop();
+			//Move(Vector2.right);
+		} else if ( Input.GetKeyUp (KeyCode.RightArrow) ){
+			Stop();
+			//Move(Vector2.right * -1);
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow) ){
+			Jump();
+		}
+
+	}
+
+
+	// Moviment Handle
+	void Jump(){
+		if(IsGrounded()) {
+			rigidBody.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse );
+		}
+	}
+
+	public void Stop(){
+		rigidBody.velocity = new Vector2 (0f,rigidBody.velocity.y);
+	}
+
+	void Move(Vector2 direction){
+
+		rigidBody.AddForce (direction * movementForce, ForceMode2D.Impulse);
+	}
+
+	bool IsGrounded() {
+		if (Physics2D.Raycast(this.transform.position, Vector2.up * -1, 0.2f, groundLayer.value)) {
+			return true;
+		}
+		else {
+			return false;
+		} 
+	}
+}
---
+++ Transmission/Assets/Scripts/Timer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public float countDown = 5f;
+	public float explosionRadius = 5f;
+	private TextMesh countDownText;
+	private float timeLeft = 0f;
+	private bool done = false;
+
+
+	void Start () {
+		timeLeft = countDown;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(done){
+			return;
+		}
+
+		timeLeft -= Time.deltaTime;
+		if (timeLeft < 0f) {
+			die ();
+			done = true;
+		} else {
+			gameObject.GetComponentInChildren<TextMesh> ().text =  ((int)timeLeft).ToString();
+		}
+	}
+
+	void die(){
+		Debug.Log (gameObject.name + " died!");
+
+		Movement script = gameObject.GetComponent<Movement> ();
+		script.die();
+		deathExplosion ();
+	}
+
+	void deathExplosion(){
+
+		RaycastHit2D[] castStar = Physics2D.CircleCastAll (transform.position, explosionRadius, Vector2.zero);
+		foreach (RaycastHit2D raycastHit in castStar) {
+
+			GameObject obj = raycastHit.collider.gameObject;
+
+
+			if(obj.tag == "Player" ){
+				Movement script = obj.GetComponent<Movement>();
+
+				script.enabled = true;
+				script.initiatePlayer();
+
+			}
+			Debug.Log(obj.name + "explosion");
+		}
+	}
+}
