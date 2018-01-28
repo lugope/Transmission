e5383e1604704986da50eb35de50dcafb47d8726
Lucas Costa <lucascostaa73@gmail.com>
2018-01-28 01:51:43 -0200

    Get any key to start Game

---
+++ Transmission/Assets/GameEventHandle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEventHandle : MonoBehaviour {
+
+	public GameObject initialPlayer;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		checkPressAnyKey();
+	}
+
+	void checkPressAnyKey(){
+
+		if ( Input.anyKeyDown ) {
+			StartGame();
+		}
+
+	}
+
+	void StartGame(){
+
+		if ( initialPlayer ) {
+
+			Movement script = initialPlayer.GetComponent<Movement>();
+			script.enabled = true;
+			script.initiatePlayer();
+		}
+
+	}
+}
